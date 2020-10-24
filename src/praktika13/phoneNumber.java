1ee48ed4bae0080736e12e795949d8c21a734a34
MeLLq <prolliki000>
2020-10-24 16:02:23 +0300

    fix phone and schet

--- src/praktika13/phoneNumber.java
+++ src/praktika13/phoneNumber.java
@@ -9,7 +9,7 @@ public class phoneNumber {
         String n = new String();
         int count = 0;
         for (int i = 0; i < a.length(); i++) {
-            if (((a.charAt(i) >= '0' && a.charAt(i) <= '9') || a.charAt(i) == '(' || a.charAt(i) == ')' || a.charAt(i) == ' ' || a.charAt(i) == '-' || a.charAt(i) == '+') && count < 11) {
+            if (((a.charAt(i) >= '0' && a.charAt(i) <= '9') || a.charAt(i) == '(' || a.charAt(i) == ')' || a.charAt(i) == ' ' || a.charAt(i) == '-' || a.charAt(i) == '+') && count < 20) {
                 if (a.charAt(i) >= '0' && a.charAt(i) <= '9') ;
                 n += String.valueOf(a.charAt(i));
                 count++;
@@ -18,10 +18,16 @@ public class phoneNumber {
                 return;
             }
         }
-        if (n.length() == 10 && n.charAt(0) == '9')
-            System.out.println("+7" + "(" + n.substring(0, 3) + ")"+n.substring(3,6)+"-"+n.substring(6,8)+"-"+n.substring(8,10));
-        else if(n.length()==11&&(n.charAt(0)=='7'||n.charAt(0)=='8'))
-        System.out.println("+7" + "(" + n.substring(1, 4) + ")"+n.substring(4,7)+"-"+n.substring(7,9)+"-"+n.substring(9,11));
+        if (n.length() == 13 && n.charAt(0) == '9')
+            System.out.println("+7" + "(" + n.substring(0, 3) + ")" + n.substring(4, 7) + "-" + n.substring(8, 10) + "-" + n.substring(11, 13));
+        else if (n.length() == 16 && (n.charAt(0) == '+' || n.charAt(0) == '8'))
+            System.out.println("+7" + "(" + n.substring(3, 6) + ")" + n.substring(7, 10) + "-" + n.substring(11, 13) + "-" + n.substring(14, 16));
+        else if (n.length() == 11 && (n.charAt(0) == '7' || n.charAt(0) == '8'))
+            System.out.println("+7" + "(" + n.substring(1, 4) + ")" + n.substring(4, 7) + "-" + n.substring(7, 9) + "-" + n.substring(9, 11));
+        else if (n.length() == 18 && (n.charAt(0) == '+' || n.charAt(0) == '8'))
+            System.out.println("+7" + "(" + n.substring(4, 7) + ")" + n.substring(9, 12) + "-" + n.substring(13, 15) + "-" + n.substring(16, 18));
+        else if (n.length() == 13 && n.charAt(0) == '8')
+            System.out.println("+7" + "(" + n.substring(3, 6) + ")" + n.substring(7, 10) + "-" + n.substring(10, 12) + "-" + n.substring(12, 13));
         else System.out.println("Не верный формат номера");
     }
 }
--- src/praktika13/schet.java
+++ src/praktika13/schet.java
@@ -1,14 +1,56 @@
 package praktika13;
+import java.util.ArrayList;
+import java.lang.String;
 
-import java.util.Scanner;
+public class schet
+{
+    String line = "Вася заработал 5006 рублей, Петя - 7563 рубля, а Маша - 300 рублей";
+    boolean check = false;
+    String a = "";
+    String number = "";
+    ArrayList<Human> human = new ArrayList<Human>();
 
-public class schet {
+    public void search()
+    {
+        for(int i = 0; i < line.length(); i++)
+        {
+            if(line.charAt(i) > 1039 && line.charAt(i) < 1072 || check)
+            {
+                check = true;
+                a += line.charAt(i);
 
-    public static void main(String[] args) {
-        String text = "Вася заработал 50000 рублей, Петя заработал - 7563 рубля, а  Маша - 30000 рублей";
-        String[] text2=text.split(" ");
-        int Vasya=Integer.parseInt(text2[2].trim());
-        int Masha=Integer.parseInt(text2[13].trim());
-        System.out.println(Masha+Vasya);
+                if(line.charAt(i + 1) == ' ' && i < line.length() - 1)
+                    check = false;
+            }
+            else if(line.charAt(i) > 47 && line.charAt(i) < 58)
+            {
+                number += line.charAt(i);
+                if(line.charAt(i + 1) == ' ' && i < line.length() - 1)
+                {
+                    human.add(new Human(a,Integer.parseInt(number)));
+                    a = "";
+                    number = "";
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        schet first = new schet();
+        first.search();
+        int sum = first.human.get(0).salary + first.human.get(2).salary;
+        System.out.println(sum + " = " + first.human.get(0).name + " + " + first.human.get(2).name);
+    }
+}
+
+class Human
+{
+    String name;
+    int salary;
+    Human(String name, int salary)
+    {
+        this.name = name;
+        this.salary = salary;
     }
 }
